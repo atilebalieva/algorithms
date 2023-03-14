c6f211171ee2e004f571ee2df748192099b023b8
aitilebalieva <aitilebalieva@gmail.com>
2023-03-14 13:52:27 -0500

    Added third solutionto last-word task.

--- last-word.js
+++ last-word.js
@@ -1,3 +1,7 @@
+function log(s) {
+  console.log(s);
+}
+
 /* Find the length of the last word. 
 Example 1:
 Input: s = "Hello World"
@@ -15,9 +19,27 @@ Output: 6
 function lengthOfLastWord(s) {
   return s.trim().split(" ").at(-1).length;
 }
+
  */
 
-/* Solution 2 
+/* Solution 2 without any build-in methods 
+
+function lengthOfLastWord(s) {
+  let word = "";
+  let wordArr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] != " ") {
+      word += s[i];
+    } else if (word.length > 1) {
+      wordArr.push(word);
+      word = "";
+    }
+  }
+  return wordArr[wordArr.length - 1].length;
+}
+*/
+
+/* Solution 3 without trim() method
 
 function lengthOfLastWord(s) {
   let deleteSpaces = s.split(" ").filter((item) => {
